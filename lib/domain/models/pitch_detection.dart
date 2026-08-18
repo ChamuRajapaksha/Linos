bea64e1fa8a1b55f83aa5b93300b29b802530f3f
Chamu <chamuditha.rajapaksha@gmail.com>
2026-08-18 14:01:03 +0530

    feat: carry snrDb through pitch detection model

--- lib/domain/models/pitch_detection.dart
+++ lib/domain/models/pitch_detection.dart
@@ -8,6 +8,7 @@ class PitchDetection {
     required this.centsOffset,
     required this.confidence,
     required this.rms,
+    this.snrDb = 0.0,
   });
 
   final Frequency frequency;
@@ -16,6 +17,9 @@ class PitchDetection {
   final double confidence;
   final double rms;
 
+  /// Peak-vs-noise-floor ratio in dB for the window that produced this reading.
+  final double snrDb;
+
   @override
   bool operator ==(Object other) {
     return other is PitchDetection &&
@@ -23,10 +27,11 @@ class PitchDetection {
         other.note == note &&
         other.centsOffset == centsOffset &&
         other.confidence == confidence &&
-        other.rms == rms;
+        other.rms == rms &&
+        other.snrDb == snrDb;
   }
 
   @override
   int get hashCode =>
-      Object.hash(frequency, note, centsOffset, confidence, rms);
+      Object.hash(frequency, note, centsOffset, confidence, rms, snrDb);
 }
