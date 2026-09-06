8d202c26bbec36386aae9784ee170ce94b49fd93
Chamu <chamuditha.rajapaksha@gmail.com>
2026-09-07 01:19:38 +0530

    feat(ui): add reusable press-scale micro-interaction wrapper

--- lib/ui/features/tuner/views/tuner_view.dart
+++ lib/ui/features/tuner/views/tuner_view.dart
@@ -1273,6 +1273,57 @@ class _ChoiceChipButton extends StatelessWidget {
   }
 }
 
+class _PressScale extends StatefulWidget {
+  const _PressScale({
+    required this.child,
+    this.pressedScale = 0.96,
+    this.pressedOpacity = 1,
+  });
+
+  final Widget child;
+  final double pressedScale;
+  final double pressedOpacity;
+
+  @override
+  State<_PressScale> createState() => _PressScaleState();
+}
+
+class _PressScaleState extends State<_PressScale> {
+  bool _pressed = false;
+
+  void _setPressed(bool value) {
+    if (_pressed != value) {
+      setState(() => _pressed = value);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final Duration duration = _pressDuration(context);
+    return Listener(
+      onPointerDown: (_) => _setPressed(true),
+      onPointerUp: (_) => _setPressed(false),
+      onPointerCancel: (_) => _setPressed(false),
+      child: AnimatedScale(
+        scale: _pressed ? widget.pressedScale : 1,
+        duration: duration,
+        curve: Curves.easeOut,
+        child: AnimatedOpacity(
+          opacity: _pressed ? widget.pressedOpacity : 1,
+          duration: duration,
+          child: widget.child,
+        ),
+      ),
+    );
+  }
+}
+
+Duration _pressDuration(BuildContext context) {
+  return MediaQuery.disableAnimationsOf(context)
+      ? Duration.zero
+      : const Duration(milliseconds: 90);
+}
+
 Duration _animDuration(BuildContext context) {
   return MediaQuery.disableAnimationsOf(context)
       ? Duration.zero
